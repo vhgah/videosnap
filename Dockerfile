a5b6fe1070455e43bc6148d932498ee8876aa608
HoangAnh <vhgah98@gmail.com>
2025-03-13 20:58:32 +0700

    update SEO

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18.20-alpine
+FROM node:18.20-alpine AS base
 
 WORKDIR /app
 
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'videosnap';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Snap Images from YouTube Videos - Instant Screenshot Tool';
+export const SITE_DESCRIPTION = 'Snap images from YouTube videos instantly. Capture multiple high-quality screenshots in seconds with our easy, hassle-free tool. Try it now!';
