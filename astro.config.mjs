6e5c74fd4b04b4892ed2b0eb7b4739143ccf8f30
HoangAnh <vhgah98@gmail.com>
2025-03-14 23:49:57 +0700

    set default url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'http://videosnap.docker',
+	site: 'https://videosnap.org',
 	integrations: [mdx(), sitemap()],
 	devToolbar: {
 		enabled: false
